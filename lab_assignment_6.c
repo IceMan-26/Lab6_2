150821a424d0d5d394cd6e39153d2bf9f608e1df
IceMan-26 <134019738+IceMan-26@users.noreply.github.com>
2023-06-23 18:51:41 -0400

    commit

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,21 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+	if(low > high)
+		return -1;
+
+	int mid = (low + high) / 2;
+
+	if (numbers[mid] == value)
+		return mid;
+	else if(numbers[mid] < value)
+		return search(numbers, mid + 1, high, value);
+	else if(numbers[mid] > value)
+		return search(numbers, low, mid -1, value);
+	else
+		return mid;
 }
 
 void printArray(int numbers[], int sz)
